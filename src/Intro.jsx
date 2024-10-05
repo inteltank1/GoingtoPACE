ecc946ae74052719c12b10479e4fa9a48b74320d
inteltank1 <inteltank@gmail.com>
2024-10-05 10:52:17 +0200

    centered PACE text on screen

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -1,14 +1,16 @@
-import anim from './assets/PACE_BeautyPass__camera02_YT1080.gif'
+import anim from "./assets/PACE_BeautyPass__camera02_YT1080.gif";
 
-function Animation(){
-    return(
-        <>
-        <div className='container'>
-        <img id='HiHello' src={anim}></img>
-        <div className='centered'><b>PACE</b></div>
+function Animation() {
+  return (
+    <>
+      <div className="container">
+        <img id="HiHello" src={anim}></img>
+        <div className="centered">
+          <b>PACE</b>
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
 
 export default Animation;
