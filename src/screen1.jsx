55401de7e79500110984b2a75abf6e38cc2b37a6
Escartin12 <adriescartin@gmail.com>
2024-10-06 12:31:03 +0200

    HTML removal of third image and addition of map

--- src/screen1.jsx
+++ src/screen1.jsx
@@ -1,6 +1,7 @@
 import image from "./assets/Construction_upscaled.jpg";
 import image2 from "./assets/Construction-2.jpg";
 import image3 from "./assets/REAL-Construction-3.jpg";
+import image4 from "./assets/flat-Map2.png";
 
 function Pantalla() {
   return (
@@ -10,17 +11,15 @@ function Pantalla() {
         <h1 id="textNextToScreen">
           What is PACE?<br></br>
           <p className="littleText">
-            Find out <a href="../WhatIsPACE">HERE</a>
+            Find out <a href="../WhatIsPACE" className="hover-link">HERE</a>
           </p>
         </h1>
         <img src={image2} id="PACE_Img2"></img>
         <h1 id="textNextToScreen2">
-          What are its parts?
-          <p className="littleText">Hola</p>
+         Look at the Plankton Map!
+         <img src={image4} id="FlatMap"></img>          
         </h1>
 
-        <img src={image3} id="PACE_Img3"></img>
-        <h1 id="textNextToScreen3">Last but not least...</h1>
         <br></br>
       </div>
     </>
