e223db9e0077c3b9a0a6c04608be6d7189c3d00b
Escartin12 <adriescartin@gmail.com>
2024-10-06 15:03:45 +0200

    Finished website

--- src/WhatIsPACE/WhatIsPACE.jsx
+++ src/WhatIsPACE/WhatIsPACE.jsx
@@ -3,84 +3,84 @@ function WhatIsPACE() {
     <div id="theDIV">
       <h1 id="bigH1">What is PACE?</h1>
       <hr id="StyledHR"></hr>
+     
       <p className="literal_yapping">
-        <p className="left">
-          <img
-            className="left2"
-            src="src/assets/pace-spacecraft-beauty2-jpg_1_-removebg-preview.png"
+        
+          <img className="IzqImg" src="src/assets/pace-spacecraft-beauty2-jpg_1_-removebg-preview.png"
             height={200}
           ></img>
-          Imagine a big, <i>friendly</i> spaceship that flies around Earth, but
+        <p className="IzqPar">  Imagine a big, <i>friendly</i> spaceship that flies around Earth, but
           instead of looking for aliens, it's looking for tiny, tiny things
           called
           <b> plankton</b>! That spaceship is called "PACE".
         </p>
         <br></br>
-        <p className="right">
-          <img src="src/assets/primero.png" className="right2"></img> Remember
+       
+          <img src="src/assets/primero.png" className="DerImg"></img>
+           <p className="DerPar"> Remember
           Plankton from SpongeBob SquarePants? He's a <sup>tiny, </sup>
           <mark id="mark">green</mark>
           &nbsp;creature that always wants the Krabby Patty formula. Well,
-          plankton in real life are a bit like Plankton, but they're
-          <b>much smaller</b> and live in
+          plankton in real life are a bit like Plankton, but they're 
+          <b> much smaller</b> and live in
           <b> the ocean</b>.
         </p>
         <br></br>
-        <p className="left">
+        
           <img
-            className="left2"
+            className="IzqImg2"
             src="src/assets/Sahara_400x300-removebg-preview.png"
           ></img>{" "}
-          PACE is a very special spaceship because it can see things that we
+         <p className="IzqPar2"> PACE is a very special spaceship because it can see things that we
           can't from <i>Earth</i>. It can see tiny things floating in the air
           called <b>aerosols</b>. Aerosols are like tiny clouds, and they can
           affect how the sun's light gets to the ocean.
         </p>
         <br></br>{" "}
-        <p className="right">
+        
           <img
-            className="right2"
+            className="DerImg2"
             src="src/assets/aba5bdbb6a5985ffdf4a73d1729b609a-removebg-preview.png"
           ></img>
-          When PACE looks at the ocean, it can see how the
+         <p className="DerPar2"> When PACE looks at the ocean, it can see how the
           <u>plankton and the aerosols are working together</u>. It's like a
           <u>detective solving a puzzle! </u>
           By understanding how plankton and aerosols work, we <i>can</i> learn
           more about our planet and <b>keep it healthy</b>.
         </p>
       </p>
-      <h1 id="big">
+     <section class="elephant"> <h1 id="big">
         The elephant in the room... Is it <b>USEFUL for you?</b>
-      </h1>
+      </h1></section>
       <p className="literal_yapping">
-        <p className="left">
+        
           <img
-            className="left2"
+            className="IzqImg3"
             src="src/assets/istockphoto-1405404817-612x612-removebg-preview.png"
             id="particPhoto"
           ></img>
-          Have you ever had very bad allergies? So bad that you have had to miss
+         <p className="IzqPar3"> Have you ever had very bad allergies? So bad that you have had to miss
           school? Well, dust, which is a type of aerosols, can make you have
           allergies. The PACE satellite will help measure dust, which can help
           everyone on planet Earth, ensuring they don't get allergic!
         </p>
         <br></br>
-        <p className="right">
+       
           <img
-            className="right2"
+            className="DerImg3"
             src="src/assets/5e9981374ee6b-removebg-preview.png"
           ></img>{" "}
-          Now, I'm SURE everyone reading this loves fish. Measuring how many
+        <p className="DerPar3">   Now, I'm SURE everyone reading this loves fish. Measuring how many
           planktons there are, you can now how many fishes there are. They also
           give you oxygen, which helps you breathe!
         </p>
         <br></br>
-        <p className="left">
+        
           <img
-            className="left2"
+            className="IzqImg4 "
             src="src/assets/images-removebg-preview.png"
           ></img>
-          So, while Plankton from SpongeBob may not be great, real planktons are
+        <p className="IzqPar4">  So, while Plankton from SpongeBob may not be great, real planktons are
           great living things!
         </p>
       </p>
